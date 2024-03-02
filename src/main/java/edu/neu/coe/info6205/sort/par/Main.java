2959700a1696cfbc64f2fe62b35315429f231984
Tarun Angrish <angrish.t@northeastern.edu>
2024-03-01 23:35:31 -0500

    [Assignment-5] Parallel sort implementation and report added

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -17,28 +17,37 @@ import java.util.concurrent.ForkJoinPool;
 public class Main {
 
     public static void main(String[] args) {
+        for(int p=2; p<=128; p*=2){
+            test(new String[]{"-N", Integer.toString(10000), "-P", Integer.toString(p)});
+        }
+    }
+
+    public static void test(String[] args) {
         processArgs(args);
-        System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
+        int noOfThread = configuration.get("-P");
+        int cutOff = configuration.get("-N");
+        ParSort.maxDepth = Math.log(noOfThread)/Math.log(2);
+        ParSort.threadPool = new ForkJoinPool(noOfThread);
+        System.out.println("Degree of parallelism: " + ParSort.threadPool.getParallelism());
         Random random = new Random();
-        int[] array = new int[2000000];
+        int[] array = new int[5120000];
         ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
-            ParSort.cutoff = 10000 * (j + 1);
-            // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
+        for (int j = 1; j < 1000 ; j*=2) {
+            ParSort.cutoff = cutOff * j ;
             long time;
             long startTime = System.currentTimeMillis();
             for (int t = 0; t < 10; t++) {
                 for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-                ParSort.sort(array, 0, array.length);
+                ParSort.sort(array, 0, array.length, 0);
             }
             long endTime = System.currentTimeMillis();
             time = (endTime - startTime);
             timeList.add(time);
 
-
-            System.out.println("cutoff：" + (ParSort.cutoff) + "\t\t10times Time:" + time + "ms");
-
+            // System.out.println((ParSort.cutoff) + "\t" + noOfThread +"\t"+ ParSort.maxDepth +"\t" + (double)time/10);
+            System.out.println((ParSort.cutoff) + "\t" + (double)time/10);
         }
+
         try {
             FileOutputStream fis = new FileOutputStream("./src/result.csv");
             OutputStreamWriter isr = new OutputStreamWriter(fis);
@@ -64,18 +73,18 @@ public class Main {
     }
 
     private static String[] processArg(String[] xs) {
-        String[] result = new String[0];
+        String[] result = new String[xs.length-2];
         System.arraycopy(xs, 2, result, 0, xs.length - 2);
         processCommand(xs[0], xs[1]);
         return result;
     }
 
     private static void processCommand(String x, String y) {
-        if (x.equalsIgnoreCase("N")) setConfig(x, Integer.parseInt(y));
+        if (x.equalsIgnoreCase("-N")) setConfig(x, Integer.parseInt(y));
         else
-            // TODO sort this out
-            if (x.equalsIgnoreCase("P")) //noinspection ResultOfMethodCallIgnored
-                ForkJoinPool.getCommonPoolParallelism();
+        if (x.equalsIgnoreCase("-P")) {
+            setConfig(x, Integer.parseInt(y));
+        }
     }
 
     private static void setConfig(String x, int i) {
@@ -84,6 +93,5 @@ public class Main {
 
     @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
     private static final Map<String, Integer> configuration = new HashMap<>();
-
-
 }
+
--- src/main/java/edu/neu/coe/info6205/sort/par/ParSort.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/ParSort.java
@@ -2,6 +2,7 @@ package edu.neu.coe.info6205.sort.par;
 
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
 
 /**
  * This code has been fleshed out by Ziyao Qiao. Thanks very much.
@@ -10,16 +11,17 @@ import java.util.concurrent.CompletableFuture;
 class ParSort {
 
     public static int cutoff = 1000;
+    public static double maxDepth = 0;
+    public static ForkJoinPool threadPool;
 
-    public static void sort(int[] array, int from, int to) {
-        if (to - from < cutoff) Arrays.sort(array, from, to);
+    public static void sort(int[] array, int from, int to, int depth) {
+        if (to - from < cutoff || depth >= maxDepth)
+            Arrays.sort(array, from, to);
         else {
-            // FIXME next few lines should be removed from public repo.
-            CompletableFuture<int[]> parsort1 = parsort(array, from, from + (to - from) / 2); // TO IMPLEMENT
-            CompletableFuture<int[]> parsort2 = parsort(array, from + (to - from) / 2, to); // TO IMPLEMENT
+            CompletableFuture<int[]> parsort1 = parsort(array, from, from + (to - from) / 2, depth+1);
+            CompletableFuture<int[]> parsort2 = parsort(array, from + (to - from) / 2, to, depth+1);
             CompletableFuture<int[]> parsort = parsort1.thenCombine(parsort2, (xs1, xs2) -> {
                 int[] result = new int[xs1.length + xs2.length];
-                // TO IMPLEMENT
                 int i = 0;
                 int j = 0;
                 for (int k = 0; k < result.length; k++) {
@@ -37,20 +39,20 @@ class ParSort {
             });
 
             parsort.whenComplete((result, throwable) -> System.arraycopy(result, 0, array, from, result.length));
-//            System.out.println("# threads: "+ ForkJoinPool.commonPool().getRunningThreadCount());
+            // System.out.println("# threads: "+ threadPool.getRunningThreadCount());
             parsort.join();
         }
     }
 
-    private static CompletableFuture<int[]> parsort(int[] array, int from, int to) {
+    private static CompletableFuture<int[]> parsort(int[] array, int from, int to, int depth) {
         return CompletableFuture.supplyAsync(
                 () -> {
                     int[] result = new int[to - from];
-                    // TO IMPLEMENT
                     System.arraycopy(array, from, result, 0, result.length);
-                    sort(result, 0, to - from);
+                    sort(result, 0, to - from, depth);
                     return result;
-                }
+                }, threadPool
         );
     }
 }
+
